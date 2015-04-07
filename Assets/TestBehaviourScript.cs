bcdd113dc8c3228122735450229cffe74cce92a6
Artsuom <artsuo@gmail.com>
2015-04-07 10:49:23 +0800

    First commit

---
+++ Assets/TestBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBehaviourScript : MonoBehaviour {
+
+	public Transform playerTransform;
+	//public int num = 1;
+	//public Component[] components;
+
+	// Use this for initialization
+	void Start () {
+		//transform.position.x += 1;
+		//transform.position.y += 1; 
+		//components = GetComponents();
+		//int result = num * 2;
+		//Debug.Log (result);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Movement
+
+	}
+}
